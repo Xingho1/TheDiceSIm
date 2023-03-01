e30aed028d120c511fae4c19c06ebff121152416
Jonak <jonakp.thakuria@gmail.com>
2023-03-01 13:31:59 +0630

    "Added gui platform, but desliding method left"

--- functions.py
+++ functions.py
@@ -6,12 +6,12 @@ def roll():
     value = [1,2,3,4,5,6]
     result = []
     for x in range(num_dice):
-        result.append([random.choice(value)])
+        result.append(random.choice(value))
     return result
 
-def config(n):
+def config(number=1):
     global num_dice
-    num_dice = int(n)
+    num_dice = int(number)
 
 
     
---
+++ gui_program.py
@@ -0,0 +1,51 @@
+import kivy
+import functions
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+Builder.load_file("kivy stuff\design.kv")
+Window.size = (999,635)
+
+
+class body(Widget):
+
+    # def desliding(self):
+    #     l = [self.face1, self.face2, self.face3, self.face4, self.face5]
+    #     if functions.num_dice < f:
+    #         for n, t in zip(range(f,f-2,-1), l.reverse()):
+    #             t.source = ""
+    #             f -= 1
+
+    def sliding(self,*args):
+        global f
+        f = 0
+        functions.config(int(args[1]))
+        #self.add_widget(Image(source= "kivy stuff/1.png", keep_ratio= True, size_hint= (None, None) size= (200, 200),pos_hint= {"center_x": 0.5}))
+        l = [self.face1, self.face2, self.face3, self.face4, self.face5]
+        if functions.num_dice > 1:
+            for n,t in zip(range(1,functions.num_dice+1),l):
+                t.source = "kivy stuff/1.png"
+                f += 1
+        self.num_die.text = "Number of Dice in use: {}".format(functions.num_dice)
+            
+    
+    def rolling(self):
+        x = functions.roll()
+        self.face1.source = "kivy stuff/{}.png".format(x[0])
+        l = [self.face1,self.face2,self.face3,self.face4,self.face5]
+        if functions.num_dice > 1:
+            for n,t in zip(range(1,(functions.num_dice+1)),l):
+                t.source = "kivy stuff/{}.png".format(x[n-1])
+
+        
+
+class Die_roller(App):
+    def build(self):
+        Window.clearcolor = (1,1,1,1)
+        return body()
+    
+if __name__ == "__main__":
+    Die_roller().run()
